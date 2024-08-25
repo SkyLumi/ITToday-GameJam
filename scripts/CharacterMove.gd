c50060a3add989bdfff20b0c4516c1a13ea8d657
Syahrial Rafky <syahrafky08@gmail.com>
2024-08-25 19:44:17 +0700

    add interact

--- scripts/CharacterMove.gd
+++ scripts/CharacterMove.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var is_active = false
 @export var is_manusia = false
 
+@onready var all_interactions = []
+@onready var interactLabel = $"InteractionComponents/Label"
+
 func get_input():
 	if is_active:
 		var input_direction = Input.get_vector("jalan_kiri", "jalan_kanan", "jalan_depan", "jalan_belakang")
@@ -36,3 +39,20 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_interaction_area_area_entered(area: Area2D) -> void:
+	all_interactions.insert(0,area)
+	update_interactions()
+
+
+func _on_interaction_area_area_exited(area: Area2D) -> void:
+	all_interactions.erase(area)
+	update_interactions()
+	
+func update_interactions():
+	if all_interactions:
+		interactLabel.text = all_interactions[0].interact_label
+	else:
+		interactLabel.text = ""
+		
